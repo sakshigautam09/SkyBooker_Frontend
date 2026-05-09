4c4da1dbfb08f4575c70f6f308568ae54a314633
sakshigautam09 <122722826+sakshigautam09@users.noreply.github.com>
2026-05-09 14:49:18 +0530

    [Sakshi].refractored the code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,24 @@
+// export const environment = {
+//   production: true,
+//   authApiUrl:    'http://localhost:5000/api',
+//   flightApiUrl:  'http://localhost:5002/api',
+//   bookingApiUrl: 'http://localhost:5004/api',
+//   paymentApiUrl: 'http://localhost:5006/api',
+//   passengerServiceUrl: 'http://localhost:5005',
+//   seatApiUrl: 'http://localhost:5003/api',
+//   notificationServiceUrl: 'http://localhost:5007'
+// };
+
+
+
+
 export const environment = {
   production: true,
-  authApiUrl:    'http://localhost:5000/api',
-  flightApiUrl:  'http://localhost:5002/api',
-  bookingApiUrl: 'http://localhost:5004/api',
-  paymentApiUrl: 'http://localhost:5006/api',
-  passengerServiceUrl: 'http://localhost:5005',
-  seatApiUrl: 'http://localhost:5003/api',
-  notificationServiceUrl: 'http://localhost:5007'
+  authApiUrl:            'https://skybooker-gateway.onrender.com/api',
+  flightApiUrl:          'https://skybooker-gateway.onrender.com/api',
+  bookingApiUrl:         'https://skybooker-gateway.onrender.com/api',
+  paymentApiUrl:         'https://skybooker-gateway.onrender.com/api',
+  passengerServiceUrl:   'https://skybooker-gateway.onrender.com',
+  seatApiUrl:            'https://skybooker-gateway.onrender.com/api',
+  notificationServiceUrl:'https://skybooker-gateway.onrender.com'
 };
